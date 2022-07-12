fe696df311ac08dfeb82f38758402208c670b375
aledeulo <alew3bslution@gmail.com>
2022-07-12 13:16:32 +0700

    create cluster with eksctl

--- .circleci/scripts/deploy-cluster.sh
+++ .circleci/scripts/deploy-cluster.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 ENV_NAME=$1
-echo "Received $ENV_NAME"
 
-VALIDATE=$(aws eks describe-cluster --name $ENV_NAME-cluster --profile ale-udacity --query 'cluster.name')
+#Verify if the cluster already exist cluster
+VALIDATE=$(aws eks describe-cluster --name $ENV_NAME-cluster --query 'cluster.name')
 echo "$VALIDATE" > ~/validate
 if cat ~/validate | grep "$ENV_NAME-cluster"
 then
@@ -13,20 +13,35 @@ else
     echo "Attempting to create the cluster $ENV_NAME-cluster"
 fi
 
-SG=$(aws cloudformation list-exports --query "Exports[?Name==\`$ENV_NAME-SecurityGroups\`].Value" \
-             --no-paginate --output text  --profile=$2||default)
+#Deploy the cluster if not exist
 
-echo "Received SG: $SG"
+PUBLIC_SUBNETS=$(aws cloudformation list-exports --query "Exports[?Name==\`$ENV_NAME-PUB-NETS\`].Value" \
+                          --no-paginate --output text)
 
-SUBNETS=$(aws cloudformation list-exports --query "Exports[?Name==\`$ENV_NAME-SubnetIds\`].Value" \
-            --no-paginate --output text  --profile=$2||default)
+echo "Received PUBLIC_SUBNETS: $PUBLIC_SUBNETS"
 
-echo "Received Subnets: $SUBNETS"
+PRIVATE_SUBNETS=$(aws cloudformation list-exports --query "Exports[?Name==\`$ENV_NAME-PRIV-NETS\`].Value" \
+                    --no-paginate --output text)
 
-aws eks create-cluster --name $ENV_NAME-cluster \
-                --role-arn arn:aws:iam::246528985509:role/UdaEKSClusterAccess \
-                --resources-vpc-config subnetIds=$SUBNETS,securityGroupIds=$SG \
-                --profile=$2||default --region us-east-1 
+echo "Received PRIVATE_SUBNETS: $PRIVATE_SUBNETS"
 
-
-aws eks wait cluster-active --name $ENV_NAME-cluster --profile=$2||default
+eksctl create cluster \
+    --name $ENV_NAME-cluster \
+    --version 1.19
+    --vpc-private-subnets=$PRIVATE_SUBNETS \
+    --vpc-public-subnets=$PUBLIC_SUBNETS \
+    --region us-east-1 \
+    --nodegroup-name $ENV_NAME-worker \
+    --node-type t2.micro \
+    --nodes 2 \
+    --nodes-min 1 \
+    --node-max 3 \
+    --ssh-access \
+    --node-volume-size 8 \
+    --ssh-public-key Default_EKS_Access_Key \
+    --appmesh-access \
+    --full-ecr-access \
+    --alb-ingress-access \
+    --managed \
+    --asg-access \
+    --verbose 3
