28e988361f113a0c387d0acc8a84f9e4b98aa420
aledeulo <alew3bslution@gmail.com>
2022-07-18 00:06:20 +0700

    add AZ

--- .circleci/scripts/deploy-cluster.sh
+++ .circleci/scripts/deploy-cluster.sh
@@ -6,14 +6,14 @@ eksctl create cluster \
     --name $ENV_NAME-cluster \
     --version 1.19 \
     --region us-east-1 \  
-    --zones us-east-1a,us-east-1b,us-east-1c \  
+    # --zones us-east-1a,us-east-1b,us-east-1c \  
     --nodegroup-name $ENV_NAME-worker \
     --node-type t3.medium \
     --nodes 2 \
     --nodes-min 1 \
     --nodes-max 3 \
     --ssh-access \
-    --node-volume-size 10 \
+    --node-volume-size 20 \
     --ssh-public-key Default_EKS_Access_Key \
     --appmesh-access \
     --full-ecr-access \
