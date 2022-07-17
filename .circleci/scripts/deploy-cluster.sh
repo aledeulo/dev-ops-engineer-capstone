f9246172343f912734de4d881b0cfa627087960d
aledeulo <alew3bslution@gmail.com>
2022-07-17 23:22:24 +0700

    remove nodes name

--- .circleci/scripts/deploy-cluster.sh
+++ .circleci/scripts/deploy-cluster.sh
@@ -7,7 +7,6 @@ eksctl create cluster \
     --version 1.19 \
     --region us-east-1 \
     --zones=us-east-1a,us-east-1b,us-east-1c \ 
-    --nodegroup-name $ENV_NAME-worker \
     --node-type t3.medium \
     --nodes 2 \
     --nodes-min 1 \
