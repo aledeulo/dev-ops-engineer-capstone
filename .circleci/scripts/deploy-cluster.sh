a3794cc0af8084007ce5b171e759e1102c97054f
aledeulo <alew3bslution@gmail.com>
2022-07-18 00:37:50 +0700

    remove add name to the node group

--- .circleci/scripts/deploy-cluster.sh
+++ .circleci/scripts/deploy-cluster.sh
@@ -6,8 +6,6 @@ eksctl create cluster \
     --name $ENV_NAME-cluster \
     --version 1.19 \
     --region us-east-1 \  
-    # --zones us-east-1a,us-east-1b,us-east-1c \  
-    --nodegroup-name $ENV_NAME-worker \
     --node-type t3.medium \
     --nodes 2 \
     --nodes-min 1 \
